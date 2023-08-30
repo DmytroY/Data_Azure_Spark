cfc1c86437dac0051814cf22ecf44353caeda1f5
Dmytro Yakovenko <dmyto_yakovenko1@epam.com>
2023-08-30 22:47:54 +0300

    more udf

--- src/jobs/job.py
+++ src/jobs/job.py
@@ -24,24 +24,24 @@ def run_job(spark, config):
     """ Check hotels data on incorrect (null) values (Latitude & Longitude).
     For incorrect values map (Latitude & Longitude) from OpenCage Geocoding API in job on fly (Via REST API).
     Generate geohash by Latitude & Longitude using one of geohash libraries (like geohash-java) with 4-characters length in extra column.
+    
     Left join weather and hotels data by generated 4-characters geohash (avoid data multiplication and make you job idempotent)"""
 
-    # read hotels to dataframe
-    df = _extract(spark, config)
+    # extract data
+    df_h = spark.read.option("header", True).csv(f"{config.get('source_data_path')}/hotels")
+    df_w = spark.read.option("header", True).parquet(f"{config.get('source_data_path')}/weather")
 
-    # Update where Latitudes/Longitudes are absent
-    print("======== updating Latitude/Longitude")
-    df_upd = df.withColumn('Latitude', when(df.Latitude.cast("int").isNull(), get_coordinates_udf(df.Country, df.City, df.Address)[0]) \
-            .otherwise(df.Latitude)) \
-        .withColumn('Longitude', when(df.Longitude.cast("int").isNull(), get_coordinates_udf(df.Country, df.City, df.Address)[1]) \
-            .otherwise(df.Longitude))
+
+    # Update hotels if Latitudes/Longitudes are absent
+    df_h1 = df.withColumn('Latitude', when(df_h.Latitude.cast("int").isNull(), get_coordinates_udf(df_h.Country, df_h.City, df_h.Address)[0]) \
+            .otherwise(df_h.Latitude)) \
+        .withColumn('Longitude', when(df_h.Longitude.cast("int").isNull(), get_coordinates_udf(df_h.Country, df_h.City, df_h.Address)[1]) \
+            .otherwise(df_h.Longitude))
     
-    # add column with geohash
-    print("======== adding GEOHASH")
-    df_hash = df_upd.withColumn("geohash", get_geohash4_udf(df_upd.Latitude, df_upd.Longitude))
+    # add column with geohash to hotels
+    df_h_hash = df_h1.withColumn("geohash", get_geohash4_udf(df_h1.Latitude, df_h1.Longitude))
 
-    print(df_hash.show(20))
+    # add column with geohash to weather
+    df_w_hash = df_w.withColumn("geohash", get_geohash4_udf(df_w.lat, df_w.lng))
 
-    print("============== Job is done ==========")
-
-
+    #
--- src/shared/udfs.py
+++ src/shared/udfs.py
@@ -1,6 +1,5 @@
 """ User defined functions """
 import os
-import time
 import geohash2
 from pyspark.sql.functions import udf
 from pyspark.sql.types import FloatType, ArrayType, StringType
@@ -9,8 +8,8 @@ from opencage.geocoder import OpenCageGeocode
 
 def _get_coordinates(country, city, address) -> list:
     """ get coordinates by addres. Powered by https://opencagedata.com/ API.
-    Usage: get_coordinates("<Counry, city, address>"),
-    for example: coordinates("US,Lavonia,890 Ross Pl")
+    Usage: _get_coordinates("<Counry, city, address>"),
+    for example: _get_coordinates("US,Lavonia,890 Ross Pl")
     returns List with coordinates [latitude, longitude], for example: [34.4454386, -83.1197032]
     """
     geocoder = OpenCageGeocode(os.getenv('OPENCAGE_API_KEY'))
@@ -21,6 +20,25 @@ def _get_coordinates(country, city, address) -> list:
         return result
     print("!!! =========== No coordinates for: ", adr)
     return ([0,0])
+
+def _get_address(lat, lon):
+    """ get city and country by coordinates. Powered by https://opencagedata.com/ API.
+    Usage: _get_address(latitude, longitude),
+    for example _get_address(34.4454386, -83.1197032),
+    returns List with city and country code: ['Lavonia', 'US']
+    """
+    geocoder = OpenCageGeocode(os.getenv('OPENCAGE_API_KEY'))
+    results = geocoder.reverse_geocode(float(lat), float(lon))
+    
+    if results[0]['components'].get('city'):
+        city = results[0]['components'].get('city')
+    else:
+        city = results[0]['components'].get('town')
+
+    country = results[0]['components'].get('country_code').upper()
+    
+    return(list((city, country)))
+
     
 def _get_geohash4(lat, lon) -> str:
     """ get 4 first simbols of geohash by coordinates. Powered by geohash2 PyPi libriary
@@ -32,4 +50,5 @@ def _get_geohash4(lat, lon) -> str:
     return result[0:4]
 
 get_coordinates_udf = udf(_get_coordinates, ArrayType(FloatType()))
+# get_address_udf = udf(_get_address,)
 get_geohash4_udf = udf(_get_geohash4, StringType())
